0d95cbbae8bb5211164ab6ec954c31a44f02b564
Aarni Koskela <akx@iki.fi>
2018-05-30 12:46:08 +0300

    Also print beeriest day and longest detox

--- streaks.py
+++ streaks.py
@@ -3,12 +3,16 @@ import sys
 from beerutil import group_by_day
 
 
-def calculate_streaks(checkins):
-    by_day = group_by_day(checkins, fill=True)
+def calculate_streaks(by_day, unstreaks=False):
     streak = None
     streaks = []
     for date, checkins in sorted(by_day.items()):
-        if len(checkins) > 0:
+        if bool(checkins) != unstreaks:
+            # The comparison above is a bit weird, but it basically hinges on
+            # empty checkin lists being falsy in a boolean context.
+            # If we're looking for unstreaks, days with checkins trigger a new
+            # unstreak.  If we're looking for streaks, the days without checkins
+            # trigger a new streak.
             if not streak:
                 streak = []
                 streaks.append(streak)
@@ -16,6 +20,14 @@ def calculate_streaks(checkins):
         else:
             streak = None
     return streaks
+
+
+def statsify_streak(streak):
+    return {
+        'length': len(streak),
+        'count': sum(len(checkins) for (date, checkins) in streak),
+        'streak': streak,
+    }
 
 
 def print_streak(name, streak_with_stats):
@@ -28,22 +40,21 @@ def print_streak(name, streak_with_stats):
 
 
 def main():
-    data = json.load(sys.stdin)
-    streaks = calculate_streaks(data)
+    checkins = json.load(sys.stdin)
+    by_day = group_by_day(checkins, fill=True)
+    streaks = calculate_streaks(by_day)
+    unstreaks = calculate_streaks(by_day, unstreaks=True)
 
-    streaks_with_stats = [
-        {
-            'length': len(streak),
-            'count': sum(len(checkins) for (date, checkins) in streak),
-            'streak': streak,
-        }
-        for streak
-        in streaks
-    ]
+    streaks_with_stats = [statsify_streak(streak) for streak in streaks]
+    unstreaks_with_stats = [statsify_streak(unstreak) for unstreak in unstreaks]
 
     print_streak('Longest streak', max(streaks_with_stats, key=lambda s: s['length']))
     print_streak('Beeriest streak', max(streaks_with_stats, key=lambda s: s['count']))
     print_streak('Most efficient streak', max(streaks_with_stats, key=lambda s: s['count'] / s['length']))
+    if unstreaks_with_stats:
+        print_streak('Longest detox', max(unstreaks_with_stats, key=lambda s: s['length']))
+    beeriest_day = max(by_day.items(), key=lambda p: len(p[1]))
+    print(f'Beeriest day: {beeriest_day[0]}, {len(beeriest_day[1])} beers')
 
 
 if __name__ == '__main__':
